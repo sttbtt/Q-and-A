4b0397aa58672d845a19158ff67db5c8459879ba
Scott Bennett <stt.btt@gmail.com>
2018-09-10 08:56:12 -0400

    Initial Commit

---
+++ Q-and-A/View Controllers/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Q-and-A
+//
+//  Created by Scott Bennett on 9/10/18.
+//  Copyright © 2018 Scott Bennett. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
